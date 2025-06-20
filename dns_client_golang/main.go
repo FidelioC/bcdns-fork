f2826f21e989cfcc514bba16e28e4feba4751fa8
FidelioC <fiocheah08@gmail.com>
2025-06-20 11:37:49 -0500

    test node

--- dns_client_golang/main.go
+++ dns_client_golang/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"strconv"
 	"strings"
@@ -109,32 +110,32 @@ listenerLoop:
 }
 
 func main() {
-	// // Command line args
-	// var eval, assetEval, listen, useCache bool
-	// var runs, runsPerSecond int
-	// var domain, outFile string
-	// var target *substrate.ChainSpecRes
-	// flag.StringVar(&domain, "domain", "example.com", "Domain to fetch chainspec for")
-	// flag.StringVar(&outFile, "outFile", "eval.csv", "Name of file to output eval results")
-	// flag.BoolVar(&eval, "eval", false, "Evaluate performance by running multiple times")
-	// flag.BoolVar(&assetEval, "assetEval", false, "Evaluate asset registration performance by running multiple times")
-	// flag.BoolVar(&listen, "listen", false, "Listen to events emitted by the chain")
-	// flag.BoolVar(&useCache, "useCache", false, "Use caching for interacting with the chain")
-	// flag.IntVar(&runs, "runs", 1, "Number of runs for evaluation")
-	// flag.IntVar(&runsPerSecond, "rps", 1, "Number of runs per second for evaluation")
-	// flag.Parse()
+	// Command line args
+	var eval, assetEval, listen, useCache bool
+	var runs, runsPerSecond int
+	var domain, outFile string
+	var target *substrate.ChainSpecRes
+	flag.StringVar(&domain, "domain", "example.com", "Domain to fetch chainspec for")
+	flag.StringVar(&outFile, "outFile", "eval.csv", "Name of file to output eval results")
+	flag.BoolVar(&eval, "eval", false, "Evaluate performance by running multiple times")
+	flag.BoolVar(&assetEval, "assetEval", false, "Evaluate asset registration performance by running multiple times")
+	flag.BoolVar(&listen, "listen", false, "Listen to events emitted by the chain")
+	flag.BoolVar(&useCache, "useCache", false, "Use caching for interacting with the chain")
+	flag.IntVar(&runs, "runs", 1, "Number of runs for evaluation")
+	flag.IntVar(&runsPerSecond, "rps", 1, "Number of runs per second for evaluation")
+	flag.Parse()
 
-	// if assetEval {
-	// 	registerAssets(domain, outFile, runsPerSecond, runs)
-	// } else if listen {
-	// 	listenToEvents(runs, outFile, useCache)
-	// } else {
-	// 	target = fetchSpec(domain, runs, runsPerSecond, outFile, eval, useCache)
-	// }
+	if assetEval {
+		registerAssets(domain, outFile, runsPerSecond, runs)
+	} else if listen {
+		listenToEvents(runs, outFile, useCache)
+	} else {
+		target = fetchSpec(domain, runs, runsPerSecond, outFile, eval, useCache)
+	}
 
-	// // 1) create verifying network
-	// fmt.Println("TEST TARGET")
-	// fmt.Printf("\n%+v\n", target)
+	// 1) create verifying network
+	fmt.Println("TEST TARGET")
+	fmt.Printf("\n%+v\n", target)
 
 	// 2) api call - client have verifying network nodes connect to the target, list of boot nodes
 		// here, the verifying network will do logics to verify the boot nodes metadata
--- dns_client_golang/verifying_network/node.go
+++ dns_client_golang/verifying_network/node.go
@@ -1,13 +1,9 @@
 package verifying_network
 
 import (
-	"bufio"
 	"context"
 	"fmt"
 	"log"
-	"os"
-	"strings"
-	"time"
 
 	// https://pkg.go.dev/github.com/libp2p/go-libp2p#section-readme
 
@@ -109,62 +105,4 @@ func (vn *VerifierNode) SendMessage(ctx context.Context, message string) {
 	if err != nil {
 		log.Println("Error publishing message:", err)
 	}
-}
-
-/* mostly used for testing and running a single node */
-func RunSingleVerifier(bootstrap string){
-	ctx := context.Background()
-	vn := NewVerifierNode(ctx, bootstrap)
-	vn.PrintHostInfo()
-	vn.ListenForMessages(ctx)
-
-	// Simple CLI loop to send messages to other nodes
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("Type messages to send to other nodes:")
-	for scanner.Scan() {
-		text := scanner.Text()
-		if strings.TrimSpace(text) == "" {
-			continue
-		}
-		vn.SendMessage(ctx, fmt.Sprintf("%s: %s", vn.host.ID().ShortString(), text))
-		time.Sleep(100 * time.Millisecond)
-	}
-}
-
-func CreateVerifierNetwork(n int) []*VerifierNode {
-	ctx := context.Background()
-	nodes := make([]*VerifierNode, 0, n)
-
-	// 1. Create the bootstrap node (no address needed)
-	bootstrapNode := NewVerifierNode(ctx, "")
-	nodes = append(nodes, bootstrapNode)
-	fmt.Println("Bootstrap node started")
-	bootstrapNode.PrintHostInfo()
-
-	// Allow time for the bootstrap node to start
-	time.Sleep(1 * time.Second)
-
-	// 2. Grab the bootstrap node's full multiaddress
-	var bootstrapAddr string
-	for _, addr := range bootstrapNode.host.Addrs() {
-		bootstrapAddr = addr.Encapsulate(ma.StringCast("/p2p/" + bootstrapNode.host.ID().String())).String()
-		break
-	}
-	fmt.Println("Bootstrap address:", bootstrapAddr)
-
-	// 3. Spin up remaining nodes, connecting to bootstrap
-	for i := 1; i < n; i++ {
-		node := NewVerifierNode(ctx, bootstrapAddr)
-		nodes = append(nodes, node)
-		node.PrintHostInfo()
-		time.Sleep(500 * time.Millisecond)
-		fmt.Printf("Node %d connected to bootstrap.\n", i)
-	}
-
-	// 4. Start listening for messages
-	for _, node := range nodes {
-		node.ListenForMessages(ctx)
-	}
-
-	return nodes
 }
