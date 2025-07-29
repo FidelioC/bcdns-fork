86df2c7ddd8dcde924ae3ba456c6d8c0daa7ba5c
FidelioC <fiocheah08@gmail.com>
2025-07-29 15:42:38 -0500

    test

--- dns_client_golang/verifying_network/verify_network.go
+++ dns_client_golang/verifying_network/verify_network.go
@@ -170,7 +170,7 @@ func VerifySpec(json_target string, numNodes int, mock_nodes []*VerifierNode) er
 
 func ZkSnark_prove(domain_name string, input_json_path string){
 	scripts.InitProve(domain_name)
-	prove_folder := scripts.GenerateProve(domain_name, input_json_path)
+	prove_folder := scripts.GenerateProve("tld", input_json_path)
 	scripts.VerifyProve("--tld", prove_folder)
 	scripts.Cleanup()
 }
