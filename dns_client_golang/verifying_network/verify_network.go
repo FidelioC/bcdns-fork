9ffd4de650796f2b5a73a22a2d1f029049604995
FidelioC <fiocheah08@gmail.com>
2025-07-29 13:23:07 -0500

    update input param

--- dns_client_golang/verifying_network/verify_network.go
+++ dns_client_golang/verifying_network/verify_network.go
@@ -171,7 +171,7 @@ func VerifySpec(json_target string, numNodes int, mock_nodes []*VerifierNode) er
 func ZkSnark_prove(domain_name string, input_json_path string){
 	scripts.InitProve(domain_name)
 	scripts.GenerateProve(domain_name, input_json_path)
-	scripts.VerifyProve("tld", "../proveCircomFiles-tld")
+	scripts.VerifyProve("--tld", "../proveCircomFiles-tld")
 	scripts.Cleanup()
 }
 
